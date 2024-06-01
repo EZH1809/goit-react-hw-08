688d39168bbdfeed8203fd176bdff1f1c4dc61c7
EZH1809 <stellage.ua@gmail.com>
2024-06-01 14:09:25 +0300

    add

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,20 +1,15 @@
-import { NavLink } from "react-router-dom";
-import css from './AuthNav.module.css';
-import clsx from "clsx";
+import { NavLink } from 'react-router-dom';
 
-const buildLinkClass = ({ isActive }) => {
-  return clsx(css.link, isActive && css.active);
-};
 
 export default function AuthNav() {
-    return (
-      <nav>
-        <NavLink to="/register" className={buildLinkClass}>
-          Registration
-        </NavLink>
-        <NavLink to="/login" className={buildLinkClass}>
-          Login
-        </NavLink>
-      </nav>
-    );
+  return (
+    <div>
+      <NavLink to="/register">
+        Register
+      </NavLink>
+      <NavLink  to="/login">
+        Log In
+      </NavLink>
+    </div>
+  );
 }
